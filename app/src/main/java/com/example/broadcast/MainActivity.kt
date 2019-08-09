f3569fc8e9d68fedf97da29e260149759f4104a6
Pavel <ivanovp09@gmail.com>
2019-08-09 15:51:32 +0300

    First step of music player. Possible to reproduce the song, but you can not pause it.

--- app/src/main/java/com/example/broadcast/MainActivity.kt
+++ app/src/main/java/com/example/broadcast/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.example.broadcast
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.media.MediaPlayer
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+    fun Play(view: View){
+        var mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.audio_test)
+        mediaPlayer?.start()
+    }
+    fun Stop(view:View){
+        var mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.audio_test)
+        mediaPlayer?.release()
+
+    }
+    fun Resume(view: View){
+        var mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.audio_test)
+        mediaPlayer?.start()
     }
 }
