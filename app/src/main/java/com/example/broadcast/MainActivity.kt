97646166f6b09a43fb3aceb6c7852c7dadc3f40a
Mitdia <knorre.mitya@mail.ru>
2019-08-10 13:31:56 +0300

    Some software to run server

--- app/src/main/java/com/example/broadcast/MainActivity.kt
+++ app/src/main/java/com/example/broadcast/MainActivity.kt
@@ -1,12 +1,74 @@
 package com.example.broadcast
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.net.wifi.WifiManager
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    private fun getLocalIpAddress(): String? {
+        try {
+
+            val wifiManager: WifiManager = getApplicationContext().getSystemService(Context.WIFI_SERVICE) as WifiManager
+            return ipToString(wifiManager.connectionInfo.ipAddress)
+        }
+        catch (ex: Exception) {
+            Log.e("IP Address", ex.toString())
+        }
+
+        return null
+    }
+
+    private fun ipToString(i: Int): String {
+        return (i and 0xFF).toString() + "." +
+                (i shr 8 and 0xFF) + "." +
+                (i shr 16 and 0xFF) + "." +
+                (i shr 24 and 0xFF)
+
+    }
+
+    fun main(args: Array<String>) {
+        embeddedServer(Netty, 8080) {
+            routing {
+                get("/") {
+                    call.respondText("Hello, world!", ContentType.Text.Html)
+                }
+            }
+        }.start(wait = true)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView = findViewById<TextView>(R.id.text)
+// ...
+
+// Instantiate the RequestQueue.
+        val queue = Volley.newRequestQueue(this)
+        val url = "192.168.212.102:8080"
+// Request a string response from the provided URL.
+        val stringRequest = StringRequest(Request.Method.GET, url,
+            Response.Listener<String> {response -> textView.text = response.substring(0, 20)},
+            Response.ErrorListener { textView.text = "That didn't work!" })
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest)
+
     }
 }
