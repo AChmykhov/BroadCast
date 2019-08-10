6ed2f73e52c25a73d5a1debe9a78ce875a78f169
savukhin <savukhin@gmail.com>
2019-08-10 10:08:24 +0300

    Created Exit Button

--- app/src/main/java/com/example/broadcast/TransmitterActivity.kt
+++ app/src/main/java/com/example/broadcast/TransmitterActivity.kt
@@ -1,12 +1,23 @@
 package com.example.broadcast
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class TransmitterActivity : AppCompatActivity() {
 
+    fun getData(): String {
+        return "123"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_transmitter)
+        findViewById<TextView>(R.id.ShowIPPortTextView).setText(getData())
+    }
+
+    fun exitFromParty(view: View) {
+        // PUT YOUR CODE HERE
     }
 }
